bf989fb339c3d2c8f3a91e58de96894bf1a17daa
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2024-01-05 20:55:39 +0530

    Create xorinthegivemrange.cpp

---
+++ xorinthegivemrange.cpp
@@ -0,0 +1,55 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+
+    int a[n];
+    for(int i=0;i<n;i++)cin>>a[i];
+
+    vector<vector<int> >frq(n,vector<int>(32,0));
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=31;j>=0;j--)
+        {
+           if(a[i]&(1<<j)) frq[i][j]++;
+        }
+    }
+    
+    for(int j=31;j>=0;j--)
+    {
+        for(int i=1;i<n;i++)
+        {
+            frq[i][j]+=frq[i-1][j];
+        }
+    }
+    
+    int q;
+    cin>>q;
+    int l,r,ans=0;
+    for(int i=0;i<q;i++)
+    {
+        cin>>l>>r;
+        ans=0;
+        if(l==0)
+        {
+            for(int j=31;j>=0;j--)
+            {
+                if((frq[r][j])%2)ans|=(1<<j);
+            }
+        }
+        else
+        {
+        	for(int j=31;j>=0;j--)
+        	{
+        		if((frq[r][j]-frq[l-1][j])%2)ans|=(1<<j);
+			}
+		}
+        cout<<ans<<" ";
+    }
+   
+    
+}
