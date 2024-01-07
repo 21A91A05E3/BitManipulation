06b74d0f0afcbe581ca653390f6a83c624650320
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2024-01-07 13:24:57 +0530

    Update Counttotalsetbits.cpp

--- Counttotalsetbits.cpp
+++ Counttotalsetbits.cpp
@@ -6,15 +6,19 @@ int nearestpow(int n)
 	while((1<<c)<=n)c+=1;
 	return c-1;
 }
-int main()
+int fun(int n)
 {
-	int n;
-	cin>>n;
 	int p=nearestpow(n);
 	int ans=0;
 	int r=p-1;
 	ans+=p*(1<<r);
 	ans+=n-(1<<p)+1;
-	ans+=nearestpow(n-(1<<p));
+	ans+=fun(n-(1<<p));
 	cout<<ans;
+}
+int main()
+{
+	int n;
+	cin>>n;
+	cout<<fun(n);
 }
